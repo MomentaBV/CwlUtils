b8511446d59a7eb63d109c898728f83c583bf72f
Drew McCormack <drewmccormack@mac.com>
2017-10-10 08:52:49 +0200

    Fixes for Swift 4

--- Sources/CwlUtils/CwlDispatch.swift
+++ Sources/CwlUtils/CwlDispatch.swift
@@ -25,7 +25,7 @@ public extension DispatchSource {
 	public class func singleTimer(interval: DispatchTimeInterval, since time: DispatchTime = DispatchTime.now(), leeway: DispatchTimeInterval = .nanoseconds(0), queue: DispatchQueue, handler: @escaping () -> Void) -> DispatchSourceTimer {
 		let result = DispatchSource.makeTimerSource(queue: queue)
 		result.setEventHandler(handler: handler)
-		result.scheduleOneshot(deadline: time + interval, leeway: leeway)
+        result.schedule(deadline: time + interval, leeway: leeway)
 		result.resume()
 		return result
 	}
@@ -42,7 +42,7 @@ public extension DispatchSource {
 	public class func repeatingTimer(interval: DispatchTimeInterval, since time: DispatchTime = DispatchTime.now(), leeway: DispatchTimeInterval = .nanoseconds(0), queue: DispatchQueue = DispatchQueue.global(), handler: @escaping () -> Void) -> DispatchSourceTimer {
 		let result = DispatchSource.makeTimerSource(queue: queue)
 		result.setEventHandler(handler: handler)
-		result.scheduleRepeating(deadline: time + interval, interval: interval, leeway: leeway)
+        result.schedule(deadline: time + interval, repeating: interval, leeway: leeway)
 		result.resume()
 		return result
 	}
@@ -61,7 +61,7 @@ public extension DispatchSourceTimer {
 	public func scheduleOneshot<T>(parameter: T, interval: DispatchTimeInterval, since time: DispatchTime = DispatchTime.now(),leeway: DispatchTimeInterval = .nanoseconds(0), handler: @escaping (T) -> Void) {
 		suspend()
 		setEventHandler { handler(parameter) }
-		scheduleOneshot(deadline: time + interval, leeway: leeway)
+        schedule(deadline: time + interval, leeway: leeway)
 		resume()
 	}
 	
@@ -69,7 +69,7 @@ public extension DispatchSourceTimer {
 	public func scheduleRepeating<T>(parameter: T, interval: DispatchTimeInterval, since time: DispatchTime = DispatchTime.now(), leeway: DispatchTimeInterval = .nanoseconds(0), handler: @escaping (T) -> Void) {
 		suspend()
 		setEventHandler { handler(parameter) }
-		scheduleRepeating(deadline: time + interval, interval: interval, leeway: leeway)
+        schedule(deadline: time + interval, repeating: interval, leeway: leeway)
 		resume()
 	}
 }
@@ -93,7 +93,8 @@ public extension DispatchTimeInterval {
 		case .milliseconds(let t): return (1.0 / Double(NSEC_PER_MSEC)) * Double(t)
 		case .microseconds(let t): return (1.0 / Double(NSEC_PER_USEC)) * Double(t)
 		case .nanoseconds(let t): return (1.0 / Double(NSEC_PER_SEC)) * Double(t)
-		}
+        case .never: return Double.greatestFiniteMagnitude
+        }
 	}
 
 	public func toNanoseconds() -> Int {
@@ -102,11 +103,10 @@ public extension DispatchTimeInterval {
 		case .milliseconds(let t): return Int(NSEC_PER_MSEC) * t
 		case .microseconds(let t): return Int(NSEC_PER_USEC) * t
 		case .nanoseconds(let t): return t
+        case .never: return Int.max
 		}
 	}
 }
-
-extension DispatchTimeInterval : Equatable {}
 
 public func ==(lhs: DispatchTimeInterval, rhs: DispatchTimeInterval) -> Bool {
     return lhs.toSeconds() == rhs.toSeconds()
